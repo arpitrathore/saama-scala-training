eebe881665b8e54ec6ed643d64272ba725b6ea1a
arpitrathore <arpit.cloud@gmail.com>
2016-10-05 09:52:34 +0530

    Added day2 examples

--- src/main/scala/org/arpit/scala/training/day2/HigherOrderFunctionTest.scala
+++ src/main/scala/org/arpit/scala/training/day2/HigherOrderFunctionTest.scala
@@ -3,22 +3,23 @@ package org.arpit.scala.training.day2
 object HigherOrderFunctionTest {
 
   def addFive(value: Int): Int = value + 5
-  
-  def divideByTwo(value: Int): Double = value.toDouble / 2
-  
-  def isEven(value: Int): Boolean = value % 2 == 0
-  
-  def processRange(start: Int, finish: Int, processor: Int => AnyVal): Unit = {
 
+  def divideByTwo(value: Int): Double = value.toDouble / 2
+
+  def isEven(value: Int): Boolean = value % 2 == 0
+
+  def processRange(start: Int, finish: Int, processor: Int => AnyVal): Unit = {
     for (i <- start to finish) {
       println(processor(i))
     }
   }
-  
+
   def main(args: Array[String]): Unit = {
-    processRange(1, 5, addFive)
-    processRange(1, 4, divideByTwo)
-    processRange(1, 6, isEven)
+    //processRange(1, 5, addFive)
+    //processRange(1, 4, divideByTwo)
+    //processRange(1, 6, isEven)
+    // processRange(1, 10, x => x*x)
+    //processRange(1, 10, _ %2 !=0)
   }
-  
+
 }
--- src/main/scala/org/arpit/scala/training/day2/LocalFunctionTest.scala
+++ src/main/scala/org/arpit/scala/training/day2/LocalFunctionTest.scala
@@ -1,11 +1,12 @@
 package org.arpit.scala.training.day2
 
 import scala.io.Source
+import java.io.InputStream
 
 //Also called nested function
 object LocalFunctionTest {
   def main(args: Array[String]): Unit = {
-    LongLinesWithLocalFunctions.processFile("/home/arathore/scala_wiki.txt", 15);
+    LongLinesWithLocalFunctions.processFile("/scala_wiki.txt", 90);
   }
 }
 
@@ -13,12 +14,13 @@ object LongLinesWithLocalFunctions {
 
   def processFile(filename: String, width: Int) {
     def processLine(line: String) {
-      if (line.length > width) {
-        println(filename + ": " + line)
+      if (line.length < width) {
+        println(line)
       }
     }
 
-    val source = Source.fromFile(filename)
+    val stream: InputStream = getClass.getResourceAsStream(filename)
+    val source = scala.io.Source.fromInputStream(stream)
     for (line <- source.getLines()) {
       processLine(line)
     }
