edcc66344f4cb68d00160ed9972812e5d0b0388d
arpitrathore <arpit.cloud@gmail.com>
2016-10-07 12:23:38 +0530

    Modified singleton example

--- src/main/scala/org/arpit/scala/training/day4/Singletons.scala
+++ src/main/scala/org/arpit/scala/training/day4/Singletons.scala
@@ -1,15 +1,25 @@
 package org.arpit.scala.training.day4
 
 object Singletons {
-  
+  def main(args: Array[String]): Unit = {
+    //val e = new Employee(1, "Arpit", 1000.00)
+
+    val e1 = Employee.getInstance
+    println(e1.getId)
+    
+    Employee.doSomeStaticThing
+  }
 }
 
-class Employee(id:Int, name:String, salary : Double)
+class Employee private (id: Int, name: String, salary: Double) {
+  def getId = id
+  override def toString = "(" + id + ", " + name + ", " + salary + ")"
+}
 
 object Employee {
-  
-  def apply(id:Int, name:String, salary : Double) = new Employee(id, name, salary)
-  def defaultEmployee = new Employee(1, "Test", 100.00)
-  
-  
+
+  val employee = new Employee(1, "Arpit", 100.00)
+  def getInstance = employee
+
+  def doSomeStaticThing = println("Doing some static thing")
 }
